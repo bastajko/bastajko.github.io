246bd27efe42258fdd918c3d9131c3455ea2a3ba
v-sbastajic <v-sbastajic@microsoft.com>
2024-11-30 01:22:57 +0100

    Adding more translations, adding translation to mobile

--- translate.js
+++ translate.js
@@ -19,7 +19,10 @@ const translations = {
       "get_to_know": "Get To Know More",
       "education": "Education",
       "explore_my": "Explore My",
-      "browse_my_recent": "Browse My Recent"
+      "browse_my_recent": "Browse My Recent",
+      "degree": "B.Sc. and M.Sc. in Computer and Electrical Engineering",
+      "backend": "Backend",
+      "frontend": "Frontend"
     },
     sr: {
       "lang": "ENG",
@@ -34,12 +37,15 @@ const translations = {
       "get_to_know_more": "Сазнајте више",
       "about_me": "О мени",
       "get_in_touch": "Контактирајте ме",
-      "download_cv": "Преузмите CV",
+      "download_cv": "Преузмите резиме",
       "contact_info": "Контакт информације",
       "get_to_know": "Сазнајте више",
       "education": "Образовање",
       "explore_my": "Истражите моје",
-      "browse_my_recent": "Прегледајте моје најновије"
+      "browse_my_recent": "Прегледајте моје најновије",
+      "degree": "Дипл. и мастер инж. Електротехнике и рачунарства",
+      "backend": "Бекенд",
+      "frontend": "Фронтенд"
     }
   };
   
